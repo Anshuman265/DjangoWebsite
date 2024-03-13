a20fdacf2dfa5785c1ef089e336f226597a054e1
Anshuman <verma77712@gmail.com>
2024-03-13 22:17:49 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here. 
+def home(request): 
+    # render function takes argument  - request 
+    # and return HTML as response 
+    return render(request, "home/home.html") 
