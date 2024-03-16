02cd4d65883a035a0ffb4f331cd0c2399749910c
Anshuman <verma77712@gmail.com>
2024-03-16 13:59:39 +0530

    changesss

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,9 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
