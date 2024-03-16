02bb7dbbd4a17792e4bd205f27b7845ebdbb3737
Anshuman <verma77712@gmail.com>
2024-03-16 13:58:38 +0530

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
-# build_files.sh
-pip install -r requirements.txt
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
 # make migrations
 python3.9 manage.py migrate 
